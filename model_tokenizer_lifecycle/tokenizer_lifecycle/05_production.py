c5c859294157621e74e49276181d0eeaeea936d2
MasemeneMatlakanaBenny <bennymasemene46@gmail.com>
2025-11-07 07:14:25 +0200

    staging the model to production

---
+++ model_tokenizer_lifecycle/tokenizer_lifecycle/05_production.py
@@ -0,0 +1,23 @@
+import mlflow
+from src.configurations_mlflow import load_host,load_tokenizer_name
+from src.configurations_mlflow import set_mlflow_experiment,set_mlflow_tracking_uri,mlflow_client
+from src.configurations import set_device,load_model
+
+# use the imported functions to create variables for model registry workflow:
+## create the device first-> gpu/cuda or cpu
+
+set_mlflow_experiment()
+set_mlflow_tracking_uri()
+device=set_device()
+
+
+tokenizer_name=load_tokenizer_name()
+model_version="1"
+
+client=mlflow_client()
+
+client.transition_model_version_stage(
+    name=tokenizer_name,
+    version=model_version,
+    stage="production"
+)
